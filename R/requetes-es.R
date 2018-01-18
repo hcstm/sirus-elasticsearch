db1bfbf68eb3dc81ed2cfb878745a3d8c152f203
Josephine <mathilde.poulhes@gmail.com>
2018-01-18 20:34:51 +0100

    corriger les requetes

--- R/requetes-es.R
+++ R/requetes-es.R
@@ -3,20 +3,40 @@ connect(es_host = "elastic-bguq8j.hackathon.insee.eu", es_port = 80)
 source("./R/utils_requete.R")
 
 # Exemple
-req <- c("RONAN ETS", "CARMINA BURANA")
-loc <- c("AGEN", "PARIS")
-res <- purrr::map2_dfr(req, loc, first_siret_desc)
-
+#req <- c("RONAN ETS", "CARMINA BURANA")
+#loc <- c("AGEN", "PARIS")
+#res <- purrr::map2_dfr(req, loc, first_siret_desc)
 req <- str_c(str_replace_na(table_rp$RS_X,replacement=""),
       str_replace_na(table_rp$ACTET_X,replacement=""),sep = " ")
+req <- str_replace_all(req,"'"," ")
+req <- str_replace_all(req,"/"," ")
+req <- str_replace_all(req,fixed(".")," ")
 
 loc  <- str_c(str_replace_na(table_rp$commune,replacement=""),
         str_replace_na(table_rp$NOMVOI_X,replacement=""),sep = " ")
+loc <- str_replace_all(loc,"'"," ")
+loc <- str_replace_all(loc,"/"," ")
+loc <- str_replace_all(loc,"-"," ")
+loc <- str_replace_all(loc,fixed(".")," ")
 
-res2 <- purrr::map2_dfr(req, loc, first_siret_desc)
+#req = req[1:200]
+#loc = loc[1:200]
+#res2 <- purrr::map2_dfr(req, loc, first_siret_desc,taille_requete=1)
+result=c()
+for(i in 1:2000){
+  result = rbind(result,first_siret_desc(req[i],loc[i]))
+}
 
-table = str_c(req,loc)
-table[table==""| table==" "]
+req <- req[i]
+loc  <- loc[i]
+q <- paste0("description:", req,"& localisation:",loc)
+res <- Search(index = 'sirus_basic_mapping', type = 'doc', q = q)
+res$hits$hits[[4]]$`_source`$sirus_id
+
+
+result_table = lapply(result,function(x)x[1:2000])
+write.csv(table_rp,file = "/Volumes/NO NAME/First_result/table_rp.csv")
+write.csv2( result,file = "/Volumes/NO NAME/First_result/result.csv")
 
 # connect()
 
