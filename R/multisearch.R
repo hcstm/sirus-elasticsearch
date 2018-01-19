5e7a0ab3d10a54ac8a0cc3ca89e28aa4b163184d
Romain Lesur <romain.lesur@gmail.com>
2018-01-19 03:44:59 +0100

    ajoute requetes multi search en R

---
+++ R/multisearch.R
@@ -0,0 +1,49 @@
+library(glue)
+library(elastic)
+library(purrr)
+library(httr)
+
+read_template <- function(file) {
+  lines <- readLines(file)
+  lines <- stringi::stri_replace_all(lines, "", regex = "\\h")
+  paste0(lines, collapse = "")
+}
+
+find_var_from_template <- function(template) {
+  stringi::stri_match_all(template, regex = "(?<=<<).+?(?=>>)")[[1]]
+}
+
+
+multi_search <- function(body) {
+  url <- paste0(elastic:::make_url(elastic:::es_get_auth()), "/_msearch")
+  tt <- httr::POST(url, 
+                   elastic:::make_up(), 
+                   elastic:::es_env$headers, 
+                   httr::content_type("application/x-ndjson"), 
+                   body = body)
+  elastic:::geterror(tt)
+  res <- elastic:::cont_utf8(tt)
+  jsonlite::fromJSON(res, TRUE)
+}
+
+ms_factory <- function(index, template_file) {
+  template <- read_template(template_file)
+  args <- find_var_from_template(template)
+  l <- vector("list", length(args))
+  names(l) <- args
+  make_unit_body <- function() {
+    paste0(sprintf('{"index" : "%s"}\n', index), 
+           glue::glue(template, .open = "<<", .close = ">>")
+    )
+  }
+  formals(make_unit_body) <- l
+  make_body <- function() {
+    liste_arguments_valeur <- mget(args)
+    lapply(liste_arguments_valeur, function(x) if(is.null(x)) stop("Il manque un argument."))
+    paste0(paste0(pmap_chr(liste_arguments_valeur, make_unit_body), collapse = "\n"), "\n")
+  }
+  formals(make_body) <- l
+
+  list(make_body = make_body)
+}
+
--- R/requetes-es.R
+++ R/requetes-es.R
@@ -38,37 +38,71 @@ result_table = lapply(result,function(x)x[1:2000])
 write.csv(table_rp,file = "/Volumes/NO NAME/First_result/table_rp.csv")
 write.csv2( result,file = "/Volumes/NO NAME/First_result/result.csv")
 
-# connect()
 
-# Quelques interrogations pour voir les index présents sur le serveur ES
-# cat_()
-# cat_aliases(index = 'sirus_basic_mapping')
-# cat_allocation()
-# cat_count(index = 'sirus_basic_mapping')
-# cat_health()
-# cat_pending_tasks()
-# cat_recovery(index = 'sirus_basic_mapping')
-# 
-# Search(index = 'sirus_basic_mapping')
-# Search(index = 'sirus_basic_mapping', fields = 'description')
-# 
-# Search(index = 'sirus_basic_mapping', body = '{
-#   "description": "retraite"
-# }')
-# 
-# 
-# Search(index = 'sirus_basic_mapping')$hits$total
-# #8526230
-# 
-# Search(index = 'sirus_basic_mapping', type = 'doc')$hits$total
-# 
-# Search(index = 'sirus_basic_mapping', type = 'doc', q= 'description:MAISON DE RETRAITE 59375 MARCHIENNES')$hits$total
-# 
-# t <- Search(index = 'sirus_basic_mapping', type = 'doc', q= 'description:MAISON DE RETRAITE 59375 MARCHIENNES', size =10, sort = '_score')
-# 
-# rostan <- Search(index = 'sirus_basic_mapping', type = 'doc', q= 'description:ROSTAN GAP 05061')
-# rostan$hits$hits[[1]]$`_source`$sirus_id
-# 
-# 
-# siret_desc("ROSTAN GAP 05061")
+
+# Requête multi search ----------------------------------------------------
+# Pour construire une requête multi search, il va nous falloir :
+# - un template de requête (ex: requete_simple.template.txt)
+# - le nom de l'index
+# - des vecteurs de chaînes de caractères pour alimenter le template : 
+#   sans valeur NULL (c'est très important). Je pense que les chaînes de caractères vides "" doivent passer
+
+# Exemple :
+source('./R/multisearch.R')
+connect()
+#connect(es_host = "elastic-bguq8j.hackathon.insee.eu", es_port = 80)
+#connect(es_host = "elastic-pl.hackathon.insee.eu", es_port = 80)
+
+# On déclare l'index sur lequel on veut travailler et on indique le chemin du template
+requete_simple <- ms_factory(index = 'sirus_basic_mapping', 
+                             template_file = './R/requete_simple.template.txt')
+
+# On construite le body :
+# La fonction make_body est automatiquement alimentée par les variables du template
+# Cela devrait éviter des oublis de variable
+body <- requete_simple$make_body(description = c("MAISON DE RETRAITE", "MAIRIE"),
+                                 localisation = c("MARCHIENNES", "MARSEILLE"))
+
+# On récupère le résultat de la requête
+res <- multi_search(body)
+
+# L'objet res est complexe
+# res$responses a autant d'enregistrements que de requêtes (2 ici)
+res$responses
+# J'ai l'impression que c'est à travers les valeurs de took et status qu'on peut 
+# savoir si une requête a fonctionné (took et status sont de même longueur que le nb de requetes) :
+res$responses$took
+res$responses$status
+# Par contre, je pense qu'il n'est pas garanti que plus bas, on ait des réponses 
+# (au cas où une requête n'aurait pas fonctionné)
+# Il serait peut être plus sûr de ne travailler que sur les status 200
+# Plus bas, on a hits avec max_score et total (nb de docs trouvés)
+res$responses$hits$max_score
+res$responses$hits$total
+# On peut ensuite descendre plus bas pour voir les retours :
+# L'objet :
+res$responses$hits$hits # est une liste de data.frame (un df par requête)
+# Si on prend un de ces data.frame :
+res$responses$hits$hits[[1]] # on constate qu'on a 10 lignes de retour et tous les champs sirus
+# Les enregistrements sont triés par la variable _score
+
+
+# Index : basic_mapping ---------------------------------------------------
+# Description : RS_X
+# Localisation : CLT_X
+# Template : requete_simple
+rp2014 <- data.table::fread(
+  file.path("./rp_final_2014.csv"), 
+  colClasses = rep("character", 52),
+  encoding = 'Latin-1')
+
+requete_simple <- ms_factory(index = 'sirus_basic_mapping', 
+                             template_file = './R/requete_simple.template.txt')
+
+body <- requete_simple$make_body(description = rp2014$RS_X,
+                                 localisation = rp2014$CLT_X)
+
+# On a un problème de temps d'exécution, même en local sur le rp 2014 (je pense qu'ES ne doit pas faire plus de 10 requêtes/seconde)
+#res <- multi_search(body)
+
 
